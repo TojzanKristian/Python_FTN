a25a454c92281e8b1cb8144e2b7d3837f99af272
Tojzán Krisztián <kristiantojzan@gmail.com>
2023-10-24 21:19:06 +0200

    Create StartWithPython.py

---
+++ StartWithPython.py
@@ -0,0 +1,127 @@
+# Types - Tipovi - Típusok
+# Numbers - Brojevi - Számok
+
+a = 5
+print(a)
+b = 3.14
+print(b)
+c = 5 + 56j
+print(c)
+d = 32.3e18
+print(d)
+print()
+
+# Bool - Tačno/Netačno - Igaz/Hamis
+T = True
+print(T)
+F = False
+print(F)
+print()
+
+#String - Tekst - Szöveg
+name = "Kristof"
+print(name)
+name1 = 'Helga'
+print(name1)
+print()
+
+# List - Lista - Lista
+H = [10, 20, 50]
+print(H)
+X = [1.67 , 3.93, 59.91]
+print(X)
+Y = X = [1 , 3.95, "Someting", True]
+print(Y)
+print()
+
+# Tuple - Torka - 
+K = (22, 44)
+print(K)
+J = (2.99, 4.487)
+print(J)
+P = (22, 4.44, True, 'OK')
+print(P)
+print()
+
+# Dictionary - Rečnik - Szótár
+G = {'Tina':55,'Gina':65,'Erik':99,'Helen':19}
+print(G)
+print()
+print()
+
+
+# Arithmetic operators - Aritmetički operatori - Aritmetikai operátorok
+# +, - , *, /, //, **, %
+
+a = 2
+b = 5
+print(a+b) # Sum - Sabiranje - Összeadás
+print(b-a) # Subtraction - Oduzimanje - Kivonás
+print(a*b) # Multiplication - Množenje - Szorzás
+print(b/a) # Division - Deljenje - Osztás
+print(b//a) # Integer division - Celebrojno deljenje - Egész számú osztás
+print(a**b) # Power - Kvadrat - Hatvány
+print(b % a) # Modulus - Moduo - Modulus
+print()
+print()
+
+
+#  - Operatori dodele vrednosti - Értékadó operátorok
+# =, +=, -=, *=, /=, **=, //=, %=
+a = 6
+print(a)
+
+a += 1
+print(a)
+
+a = 6
+a -= 1
+print(a)
+
+a = 6
+a *= 2
+print(a)
+
+a = 6
+a /= 2
+print(a)
+
+a = 6
+a **= 2
+print(a)
+
+a = 6
+a //= 2
+print(a)
+
+a = 6
+a %= 8
+print(a)
+print()
+print()
+
+
+# Comparison operators - Operátori upoređivanja - Összehasonlító operátorok
+# <, >, <=, >=, !=, ==
+a = 15
+b = 5
+
+print(a < b)
+print(a > b)
+print(a <= b)
+print(a >= b)
+print(a != b)
+print(a == b)
+print()
+print()
+
+
+# Logical operators - Logički operatori - Logikai operátorok
+# and, or, not
+a = 5
+b = 15
+
+print(a < b and a != 8)
+print(a < b or a == 5)
+print()
+print()
